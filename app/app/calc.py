9a80f0f4de7b65a2cedfc09607c6267c4278a038
Kuriakose Kuruvila Anathanam <kris.kuruvilla@gmail.com>
2023-01-04 18:11:57 +0530

    Configured docker compose and the checks to wait for db - try 8

--- app/app/calc.py
+++ app/app/calc.py
@@ -9,4 +9,3 @@ def add(x,y):
 def sub(x,y):
     '''Subtract y from x and return result.'''
     return x-y
-
--- app/app/tests.py
+++ app/app/tests.py
@@ -5,6 +5,7 @@ Sample Tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     '''Test the calc module'''
 
