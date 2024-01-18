37c63fe6d810efd4eb6626f94859860c8111941c
Kuriakose <kris.kuruvilla@gmail.com>
2024-01-18 15:41:06 +0530

    Setup custom django admin.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
-from django.contrib import admin
+"""
+Django admin customization.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin panel for the users."""
+    ordering = ["id"]
+    list_display = ["email", "name"]
+
+
+admin.site.register(models.User, UserAdmin)
